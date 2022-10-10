003145a3c85273ec07709a3b4a622d4cd4ae317b
djsreeraj <sreeraj011m@gmail.com>
2022-10-10 07:12:27 +0400

    added base url validation

--- methods/get-request.js
+++ methods/get-request.js
@@ -1,5 +1,7 @@
 module.exports = (req, res) => {
-
+    let baseURL = req.url.substring(0, req.url.lastIndexOf("/") + 1);
+    console.log(req.url)
+    console.log(baseURL);
     let id = req.url.split("/")[3];
     console.log(id);
     const regexV4 = new RegExp(
@@ -14,7 +16,7 @@ module.exports = (req, res) => {
     } else if(!regexV4.test(id)){
         res.writeHead(400, {"Content-Type": "application/json" });
         res.end(JSON.stringify({ title: "Validation Failed", message: 'UUID not valid'}))
-    } else if(regexV4.test(id)){
+    } else if(baseURL === "/api/movies/" &&  regexV4.test(id)){
         res.setHeader("Content-Type", "application/json");
         let filteredMovie = req.movies.filter((movie) => {
             return movie.id === id;
@@ -35,14 +37,4 @@ module.exports = (req, res) => {
         res.end(JSON.stringify({ title: "Not Found", message: 'Route not found'}))
     }
 
-
-    // if(req.url === "/api/movies/:id") {
-    //     res.statusCode = 200;
-    //     res.setHeader("Content-Type", "application/json");
-    //     res.write(JSON.stringify(req.movies.id));
-    //     res.end();
-    // } else {
-    //     res.writeHead(404, {"Content-Type": "application/json" });
-    //     res.end(JSON.stringify({ title: "Not Found", message: 'Route not found'}))
-    // }
 };
