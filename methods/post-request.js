2e695b8bc257159db46076c03f002e0ad41fe617
djsreeraj <sreeraj011m@gmail.com>
2022-10-11 23:58:46 +0400

    added POST request

--- methods/post-request.js
+++ methods/post-request.js
@@ -1,1 +1,23 @@
-module.exports = (req, res) => {};
+const crypto = require("crypto");
+const requestBodyParser = require("../util/body-parser");
+
+module.exports = async (req, res) => {
+    if(req.url === "/api/movies"){
+        try{
+            let body = await requestBodyParser(req);
+            body.id = crypto.randomUUID();
+            req.movies.push(body);
+            res.writeHead(201, { "Content-Type": "application/json" });
+            res.end();
+        } catch (err){
+            console.log(err);
+            res.writeHead(400, {"Content-Type": "application/json" });
+            res.end(
+                JSON.stringify({ 
+                    title: "Validation Failed", 
+                    message: 'Request body not valid'
+            }))
+        }
+    }
+
+};
